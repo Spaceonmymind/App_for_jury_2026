4507694c519724d5e822acabd047a23efe5df566
Spaceonmymind <gladkih.egor@mail.ru>
2026-03-17 17:25:35 +0500

    ipdate

--- jury_app/app/db.py
+++ jury_app/app/db.py
@@ -1,16 +1,22 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////app/data/app.db")
 
 
 class Base(DeclarativeBase):
     pass
 
 
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args,
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
